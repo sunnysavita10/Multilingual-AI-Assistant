699744711d2b6b558bc67d6ca80672b67c586d30
sunnysavita10 <engsunny3365@gmail.com>
2024-04-10 21:46:39 +0530

    code updated

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from src.helper import voice_input, llm_model_object, text_to_speech
+
+
+def main():
+    st.
---
+++ src/helper.py
@@ -0,0 +1,7 @@
+import speech_recognition as sr
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+from gtts import gTTS
+
+print("perfect!!")
