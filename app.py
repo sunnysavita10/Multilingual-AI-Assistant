1efbeb89a05074a04a2c642a87e6d7d314931b59
sunnysavita10 <engsunny3365@gmail.com>
2024-04-10 22:03:46 +0530

    code updated

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ from src.helper import voice_input, llm_model_object, text_to_speech
 
 
 def main():
-    st.
+    st.title("Multilingual AI Assistant 🤖")
+    
+    if st.button("Ask me anything"):
+        with st.spinner("Listening..."):
+            text=voice_input()
+            response=llm_model_object(text)
+            text_to_speech(response)
+            
+            
+            audio_file=open("speech.mp3","rb")
+            audio_bytes=audio_file.read()
+            
+            
+            st.text_area()
+            st.audio()
+            st.download_button()
+            
+if __name__=='__main__':
+    main()
--- src/helper.py
+++ src/helper.py
@@ -5,3 +5,19 @@ import os
 from gtts import gTTS
 
 print("perfect!!")
+load_dotenv()
+
+GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
+os.environ["GOOGLE_API_KEY"]=GOOGLE_API_KEY
+
+
+
+def voice_input():
+    pass
+
+def text_to_speech():
+    pass
+
+
+def llm_model_object():
+    pass
