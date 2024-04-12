599b68c42a359b6b0f3aaae8e7bae35b16ce4bba
sunnysavita10 <engsunny3365@gmail.com>
2024-04-12 21:45:50 +0530

    final code added

--- app.py
+++ app.py
@@ -16,9 +16,12 @@ def main():
             audio_bytes=audio_file.read()
             
             
-            st.text_area()
-            st.audio()
-            st.download_button()
+            st.text_area(label="Response:",value=response,height=350)
+            st.audio(audio_bytes)
+            st.download_button(label="Download Speech",
+                               data=audio_bytes,
+                               file_name="speech.mp3",
+                               mime="audio/mp3")
             
 if __name__=='__main__':
     main()
--- src/helper.py
+++ src/helper.py
@@ -13,11 +13,40 @@ os.environ["GOOGLE_API_KEY"]=GOOGLE_API_KEY
 
 
 def voice_input():
-    pass
+    r=sr.Recognizer()
+    
+    with sr.Microphone() as source:
+        print("listening...")
+        audio=r.listen(source)
+    try:
+        text=r.recognize_google(audio)
+        print("you said: ", text)
+        return text
+    except sr.UnknownValueError:
+        print("sorry, could not understand the audio")
+    except sr.RequestError as e:
+        print("could not request result from google speech recognition service: {0}".format(e))
+    
 
-def text_to_speech():
-    pass
+def text_to_speech(text):
+    tts=gTTS(text=text, lang="en")
+    
+    #save the speech from the given text in the mp3 format
+    tts.save("speech.mp3")
 
-
-def llm_model_object():
-    pass
+def llm_model_object(user_text):
+    #model = "models/gemini-pro"
+    
+    genai.configure(api_key=GOOGLE_API_KEY)
+    
+    model = genai.GenerativeModel('gemini-pro')
+    
+    response=model.generate_content(user_text)
+    
+    result=response.text
+    
+    return result
+    
+    
+    
+    
